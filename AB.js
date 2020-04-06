cb8360c6a78bc288d7dacb882d5ee6f762cf8109
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2020-04-06 10:28:35 -0700

    Create AB.js

---
+++ AB.js
@@ -0,0 +1,29 @@
+pair As frame used in narow 
+
+{
+text as A(12211)
+ prone As 
+ view text as number flow 
+ broght figure 
+ \A 
+ \B 
+ gor = 1 As A(12211) popups (sign!!)
+ gor = form !A flowing brings '22'
+ A brings '11' and '22' 
+ A by 1 number seq in \A
+ take gor = brings [A\A\B\B\A\A]
+  gor = 2 As B(11122) poups (nosign!!)
+  gor = form !B folowing brings '11'
+  B brings '22' and '11' 
+  B by 1 number seq in \B
+  }
+  Signs (Hello) ground As Addition in the #OREOBEEM 
+  use sets 
+  of fonts of AB=1>0 on AB 
+  \stopaction
+  \startaction
+  brings Value of 
+  (1111\\\\11111\\\\111111)
+  .\[:;].\ 
+  
+  
