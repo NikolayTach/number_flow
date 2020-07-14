0172d6053cac2b742775c0be89c9116678a76928
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2020-07-14 11:55:15 +0300

    Update AB.js

--- AB.js
+++ AB.js
@@ -1,12 +1,12 @@
 pair As frame used in narow 
-
+fuction addMessage(element,message)
 {
    text as A(12211)
    prone As 
    view text as number flow 
  broght figure 
- \A 
- \B 
+ import number_flow(\A)
+ import number_flow(\B)
  gor = 1 As A(12211) popups (sign!!)
  gor = form !A flowing brings '22'
  A brings '11' and '22' 
@@ -18,8 +18,8 @@ pair As frame used in narow
   B by 1 number seq in \B
  take gor = brings [B\B\B\B\B\B]
   }
-  
-    {
+  function ABAB()    
+          {
    if
 var column = (timeoverleap) known as
 '0_111111' inherited image cycle frame
@@ -29,11 +29,12 @@ row equal to = (xsearching) or
 As
    var column = due in '0_111111' to
 row equal = (timoverleap) known as
-'0_111111' [complex&@/
-0718375299528bar]
+'0_111111' [complex&@/0718375299528bar]
 var tasking force = 'abc' for 'basics'
 var tasking force = 'up' and 'down'
 cont rows = '1_0000001' while
-rows assets [text]
-rows assets [numbers]
-}
+rows asserts(text)
+rows asserts(numbers)
+       }
+       
+   }
